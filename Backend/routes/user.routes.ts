7d0f4db5d0597ac63d7ba7d3afd95bafc996fde3
Pierre van maele <93315104+jurjerie1@users.noreply.github.com>
2025-02-27 11:45:28 +0100

    ajout de la route login

--- Backend/routes/user.routes.ts
+++ Backend/routes/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { Register } from "../controllers/UserController.js";
+import { Login, Register } from "../controllers/UserController.js";
 import { validatePost, schemas } from "../middlewares/validationMiddleware.js";
 const userRoutes = Router();
 
 userRoutes.post("/register", validatePost(schemas.register), Register);
-
+userRoutes.post("/login", validatePost(schemas.login), Login);
 export default userRoutes;
