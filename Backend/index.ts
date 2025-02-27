6734e5f567e70a54382dea77cadfa0abbab664e9
Pierre van maele <93315104+jurjerie1@users.noreply.github.com>
2025-02-27 09:17:45 +0100

    ajout de typescript

---
+++ Backend/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request: Request, response: Response) => { 
+    response.status(200).send("Hello World");
+  }); 
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
