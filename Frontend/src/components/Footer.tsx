e4e2d0c45347f191b9e7b7cf0d789587f1d13628
kyllian-claveau <kyllian.claveau@icloud.com>
2025-02-27 10:14:04 +0100

    Create Footer.tsx

---
+++ Frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer style={{ padding: '1em', backgroundColor: '#1a1a1a', color: 'white', textAlign: 'center' }}>
+            <p>&copy; 2025 Akkor Hotel ltd. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
