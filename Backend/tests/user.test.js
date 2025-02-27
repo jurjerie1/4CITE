2778a8da570e080ef9d5f6b2dc90033b7bd5d622
Pierre van maele <93315104+jurjerie1@users.noreply.github.com>
2025-02-27 10:07:45 +0100

    ajout d'autre test route register User

--- Backend/tests/user.test.js
+++ Backend/tests/user.test.js
@@ -9,8 +9,32 @@ describe("User API", () => {
             .send({
                 email: "newuser@example.com",  
                 password: "secret",
+                pseudo: "newuser",
             });
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty("token");
     });
+    // test email valide
+    it("should not register user with invalid email", async () => {
+        const res = await request(app)
+            .post("api/users/register")
+            .send({
+                email: "newuser",  
+                password: "secret",
+                pseudo: "newuser",
+            });
+        expect(res.statusCode).toEqual(400);
+    });
+    // test email déjà existant
+    it("should not register user with existing email", async () => {
+        const res = await request(app)
+            .post("api/users/register")
+            .send({
+                email: "newuser@example.com",  
+                password: "secret",
+                pseudo: "newuser",
+            });
+        expect(res.statusCode).toEqual(400);
+    });
+    
 }); // Fermeture correcte du describe
