31982f6c52ab526859f2665bfa0911d643a88f1f
Pierre van maele <93315104+jurjerie1@users.noreply.github.com>
2025-02-27 09:39:53 +0100

    ajout de la route register

--- Backend/controllers/UserController.ts
+++ Backend/controllers/UserController.ts
@@ -2,5 +2,4 @@ import { Request, Response } from 'express';
 
 export const Login = async (req: Request, res: Response) : Promise<void> => {
     const { email, password } = req.body;
-    
 }
--- Backend/index.ts
+++ Backend/index.ts
@@ -8,6 +8,10 @@ app.get("/", (request: Request, response: Response) => {
     response.status(200).send("Hello World");
   }); 
 
+app.get("/api/users/register", (request: Request, response: Response) => {
+    response.status(200).send("Register");
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     }
