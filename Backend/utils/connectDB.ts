8f804ae619039a6f1b0af2b66f4632259915cfbf
Pierre van maele <93315104+jurjerie1@users.noreply.github.com>
2025-03-09 22:05:41 +0100

    fix connexion string

--- Backend/utils/connectDB.ts
+++ Backend/utils/connectDB.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 export async function connectDB() {
     await mongoose.connect(
-        `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@localhost:27017/${process.env.MONGO_DB}?authSource=admin`
-        // `mongodb+srv://${process.env.MONGO_ID}:${process.env.MONGO_PWD}@cluster0.xzrgs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+        // `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@localhost:27017/${process.env.MONGO_DB}?authSource=admin`
+        `mongodb+srv://${process.env.MONGO_ID}:${process.env.MONGO_PWD}@cluster0.xzrgs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     )
         .then(() => {
             console.log('connecté à mongo DB')
