1f84712ad3859aaf85d75c4d95fef002b65ae9bd
kyllian-claveau <kyllian.claveau@icloud.com>
2025-03-12 15:49:28 +0100

    Add redirection after Login

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
 
@@ -7,17 +8,38 @@ const Login: React.FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://localhost:3000/api/users/login', {email, password});
             setMessage(response.data.message);
+
+            const userRole = response.data.user.role;
+
+            switch(userRole) {
+                case 2:
+                    navigate('/dashboard');
+                    break;
+                case 1:
+                    navigate('/dashboard');
+                    break;
+                case 0:
+                default:
+                    navigate('/dashboard');
+                    break;
+            }
+
+            // Stocker les informations utilisateur dans localStorage ou sessionStorage si nécessaire
+            localStorage.setItem('user', JSON.stringify(response.data.user));
+            localStorage.setItem('token', response.data.token);
+
         } catch (error: unknown) {
             if (axios.isAxiosError(error)) {
-                setMessage(error.response?.data.error || 'An error occurred');
+                setMessage(error.response?.data.error || 'Une erreur est survenue');
             } else {
-                setMessage('An unexpected error occurred');
+                setMessage('Une erreur inattendue est survenue');
             }
         }
     };
@@ -26,7 +48,7 @@ const Login: React.FC = () => {
         <div className="flex flex-col min-h-screen w-screen">
             <Header/>
             <main
-                className="flex-grow flex items-center justify-center container mx-auto px-4 pt-24"> {/* Center the content */}
+                className="flex-grow flex items-center justify-center container mx-auto px-4 pt-24"> {/* Center the common */}
                 <div className="w-full max-w-md">
                     <h2 className="text-3xl font-light text-center mb-12">Se connecter</h2>
                     <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
